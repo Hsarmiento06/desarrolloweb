e35b08993e194b2b242c07a6d31f5b88094f5412
hebert sarmiento carranza <I202418032@cibertec.edu.pe>
2024-10-13 00:06:13 +0000

    Formulario de registro terminado

--- javascript/demo01/js/app.js
+++ javascript/demo01/js/app.js
@@ -1,37 +1,48 @@
 
-const formularioregistro = document.getElementById('formulario-registro');
-const mensajeserror = document.getElementById('mensajeserror');
+const formularioregistro = document.getElementById("formulario-registro");
+const mensajeserror = document.getElementById("mensajeserror");
 
-formularioregistro.addEventListener("submit", function(event){
-
+formularioregistro.addEventListener("submit", function (event) {
     event.preventDefault();
 
     const usuario = document.getElementById("usuario").value;
     const correo = document.getElementById("correo").value;
-    const contrasena = document.getElementById("contraseña")
+    const contrasena = document.getElementById("contrasena").value;
 
-    mensajeserror.innerhtml = "";
+    mensajeserror.innerHTML = "";
 
-    if (!usuario.trim() ) {
-        monstrarError("El nombre de usuario es obligatorio");
+    if (!usuario.trim()) {
+        mostrarError("El nombre de usuario es obligatorio.");
         return;
     }
 
-    if( !correo.trim() ) {
-        monstrarError("El correo es requerido");
+    if (!correo.trim() || !esCorreoValido(correo)) {
+        mostrarError("Por favor ingresar un correo válido.")
         return;
     }
 
-    if( !contrasena.trim() ) {
-        monstrarError("La contraseña es requerida");
+    if (!contrasena.trim() || !esContrasenaCorrecta(contrasena)) {
+        mostrarError("La contraseña debe tener mínimo 8 caracteres y debe haber letras minúsculas, mayúsculas, número y símbolo.")
         return;
     }
+
+    alert("El registro es satisfactorio.");
+    formularioregistro.reset();
 
 })
 
-function monstrarError( mensaje ) {
+function mostrarError(mensaje) {
     const mensajeerror = document.createElement("div");
     mensajeerror.className = "error";
     mensajeerror.textContent = mensaje;
-    mensajeerror.appendChild(mensajeerror);
+    mensajeserror.appendChild(mensajeerror);
+}
+
+function esCorreoValido(correo) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(correo);
+}
+
+function esContrasenaCorrecta(contrasena) {
+    let patron = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/;
+    return patron.test(contrasena);
 }
